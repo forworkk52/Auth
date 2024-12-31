9dc43949eee47af03de17f680c106a203ab20424
satish chile <satish.chile@bluebenz.com>
2024-12-31 19:00:51 +0530

    First

---
+++ Models/AgeValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validation_MVC.Models
+{
+    public class AgeValidator:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is int age)
+            {
+                if (age % 2 != 0)
+                {
+                    return new ValidationResult(ErrorMessage ?? "Age must be an even number.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validation_MVC.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "plz enter Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address cannot exceed 200 characters")]
+        public string Addr { get; set; }
+
+        [Required(ErrorMessage = "Mobile number is required")]
+        [Phone(ErrorMessage = "Invalid mobile number")]
+        public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long and include one letter, one number, and one special character.")]
+        public string Password { get; set; }
+
+       
+        [Required(ErrorMessage = "Please Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfermPassword { get; set; }
+
+
+        [AgeValidator]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Credit Card Number is required")]
+        [CreditCard(ErrorMessage = "Invalid Credit Card number")]
+        public string CreditCardNumber { get; set; }
+
+        [Url(ErrorMessage = "Invalid URL")]
+        public string Url { get; set; }
+    }
+}
